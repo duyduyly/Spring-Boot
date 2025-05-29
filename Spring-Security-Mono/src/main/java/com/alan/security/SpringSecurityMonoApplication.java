b2513737d379c9777c85432b51f3fae27327bba8
Duy <duyduyly123vn@gmail.com>
2025-05-29 14:25:32 +0700

    Spring security | Project Structure

---
+++ Spring-Security-Mono/src/main/java/com/alan/security/SpringSecurityMonoApplication.java
@@ -0,0 +1,13 @@
+package com.alan.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMonoApplication.class, args);
+	}
+
+}
---
+++ Spring-Security-Mono/src/test/java/com/alan/security/SpringSecurityMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alan.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityMonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
