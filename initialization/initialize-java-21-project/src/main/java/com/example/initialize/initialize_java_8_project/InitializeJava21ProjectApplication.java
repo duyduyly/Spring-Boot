860d0b060669d19df3eff6380f99dd2b7fcf840a
Duy <duyduyly123vn@gmail.com>
2026-01-30 14:24:33 +0700

    document | initialize spring boot 3.5.10 and java 21

---
+++ initialization/initialize-java-21-project/src/main/java/com/example/initialize/initialize_java_8_project/InitializeJava21ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.initialize.initialize_java_8_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializeJava21ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializeJava21ProjectApplication.class, args);
+	}
+
+}
